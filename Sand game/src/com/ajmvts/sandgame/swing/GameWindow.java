0d38abd6b875154ac0e9f4ec554eafe157bc25df
vtsman <hokiespencer@gmail.com>
2014-07-27 20:48:35 -0400

    Paint function added

--- Sand game/src/com/ajmvts/sandgame/swing/GameWindow.java
+++ Sand game/src/com/ajmvts/sandgame/swing/GameWindow.java
@@ -1,7 +1,27 @@
 package com.ajmvts.sandgame.swing;
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GameWindow extends JFrame
 {
-
+	public GameWindow(int width, int height, String name){
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    this.setVisible(true);
+	    this.setName(name);
+	    this.setPreferredSize(new Dimension(width, height));
+	    this.setLocationRelativeTo(null);
+	}
+	
+	public void setPixel(int x, int y, int r, int g, int b){
+		this.getGraphics().setColor(new Color(r, g, b));
+	}
+	
+	/*public in[] getPixel(int x, int y){
+		Color c = this.getGraphics().
+	}*/
+	
+	
 }
