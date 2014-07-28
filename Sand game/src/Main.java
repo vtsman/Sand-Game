50876f5bb1c96ead78f777a49e4b98d8f9d5810a
ajm27 <ajm27@njit.edu>
2014-07-27 20:10:33 -0400

    Added Hello, World.

--- Sand game/src/Main.java
+++ Sand game/src/Main.java
@@ -1,9 +1,10 @@
 
-public class Main {
+public class Main 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello, World!");
 	}
 
 }
